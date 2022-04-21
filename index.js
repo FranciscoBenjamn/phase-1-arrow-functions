5cacd650072d7cc09fc86bef346c81dd73edfb75
Franklin Benjamin <franciscovibes14@gmail.com>
2022-04-21 15:39:55 -0400

    commit tree 3899e4e0dea4af462e398d8b063d395a6c4bc456

---
+++ index.js
@@ -0,0 +1,6 @@
+const divide = (parameter1, parameter2) => parameter1 / parameter2
+divide(2000,100); 
+const square = (parameter4) => parameter4 * parameter4
+square(2);
+const add = (parameter5, parameter6) => parameter5 + parameter6
+add(3,4);
